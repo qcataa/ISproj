8f678621d302a5865c0bf0e93d591991c5255da4
Adi Cutitoiu <dcutitoiu@yahoo.com>
2018-05-21 19:01:57 +0300

    Remove student birthdate

--- ISproj/Models/Student.cs
+++ ISproj/Models/Student.cs
@@ -13,7 +13,6 @@ namespace ISproj.Models
         public string Name { get; set; }
         public string Address { get; set; }
         public string Surname { get; set; }
-        public DateTime Birthdate { get; set; }
         public string CNP { get; set; }
         public string Email { get; set; }
 
